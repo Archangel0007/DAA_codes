a5b56386cc88519c739f6ab896820b7511f12919
Navaneetha Krishnan .S <113388386+Archangel0007@users.noreply.github.com>
2023-08-07 15:56:09 +0530

    Create Search_a_2D_Matrix.cpp

---
+++ Leetcode_Solutions/Search_a_2D_Matrix.cpp
@@ -0,0 +1,21 @@
+/*
+  This code solves the 79th problem of leetcode "Search a 2D Matrix"
+*/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int n = mat.size(), m = mat[0].size();
+        int i = 0, j = m*n - 1;
+        while(i<=j){
+            int mid = (i + j)/2;
+            int r = mid/m;
+            int c = mid%m;
+            if(mat[r][c]==target)return true;
+            if(mat[r][c]>target){
+                j = mid - 1;
+            }
+            else i = mid + 1;
+        }
+        return false;
+    }
+};
