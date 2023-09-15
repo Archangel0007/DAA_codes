235afd881c8d1a24b6f432ecba626dfe92ce8da8
Navaneetha Krishnan .S <113388386+Archangel0007@users.noreply.github.com>
2023-09-15 22:58:34 +0530

    Create Find_the_Maximum_Achievable_Number.cpp

---
+++ Leetcode_Solutions/Find_the_Maximum_Achievable_Number.cpp
@@ -0,0 +1,9 @@
+/*
+  This code solves the 2769th problem o leetcode "Find the Maximum Achievable Number"
+*/
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return(num+2*t);
+    }
+};
